dc69ad24d35765c0d2669e173bb13266cc4e85e6
Pankaj Singh <pankaj.sandbox@gmail.com>
2023-02-23 15:17:03 +0530

    useEffect dynamic styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import Card from './Card';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+const theme = {
+  primary: '#4caf50',
+  pinkish: 'pink'
+}
+const Button = styled.button`
+  background-color: ${props => props.length > 2 ? 'red' : props.length < 2 ? "yellow" : 'pink'}
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+`
 function App() {
   const [showCard, setshowCard] = useState(true)
-  const [cards, setCards] = useState([
-    {
-      id: "jhajhaj",
-      name: 'Afsar',
-      title: 'working',
-      avatar: "https://www.w3schools.com/howto/img_avatar.png"
-    }, {
-      id: "jhajhajaaa",
-      name: 'Himanshu',
-      title: 'working',
-      avatar: "https://www.w3schools.com/howto/img_avatar.png"
-    }
-    , {
-      id: "jhajhajakndka",
-      name: 'Ankush',
-      title: 'working',
-      avatar: "https://www.w3schools.com/howto/img_avatar.png"
-    }
-  ])
+  const [cards, setCards] = useState([])
 
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=10').then((res) => {
+      console.log(res.data.results);
+      setCards(res.data.results)
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
   const deleteCardHAndler = (cardIndex) => {
     const cards_copy = [...cards]
     cards_copy.splice(cardIndex, 1)
@@ -29,23 +37,32 @@ function App() {
   }
   const toggleShowCard = () =>
     setshowCard(!showCard)
+  // const buttonStyle = {
+  //   backgroundColor: null
+  // }
+  const classes = ['button']
+  if (cards.length < 3) classes.push('yellow')
+  if (cards.length < 2) classes.push('blue text')
 
   const changeNameHandler = (event, id) => {
-    const cardIndexs = cards.findIndex(card => card.id == id)
+    const cardIndexs = cards.findIndex(card => card.login.uuid == id)
     const copy_cards = [...cards]
-    copy_cards[cardIndexs].name = event.target.value
+    copy_cards[cardIndexs].name.first = event.target.value
     setCards(copy_cards)
   }
 
   const cardMArkup = showCard && (
     cards.map((card, index) =>
-      <Card name={card.name} title={card.title} avatar={card.avatar} key={card.id} onChangeName={(event) => changeNameHandler(event, card.id)} onDelete={() => deleteCardHAndler(index)} />
+      <Card name={card.name.first} email={card.email} avatar={card.picture.large} key={card.login.uuid}
+        // name={card.name} title={card.title} avatar={card.avatar} key={card.id}
+        onChangeName={(event) => changeNameHandler(event, card.login.uuid)} onDelete={() => deleteCardHAndler(index)} />
     )
   )
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button className="button text-center" onClick={toggleShowCard}>Toggle Show/Hide</button>
+      {/* <Button>Toggle</Button>
+      <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle Show/Hide</button> */}
       {cardMArkup}
 
       {/* <Card name='Ankush' title="Working">
--- src/Card.js
+++ src/Card.js
@@ -10,7 +10,7 @@ export const Card = (props) => {
                 <h4><b>{props.name}</b></h4>
                 <input type="text" value={props.name} onChange={props.onChangeName}/>
                 <p><button className="button button-red" onClick={props.onDelete}>Delete  </button></p>
-                <p>Work Status: {props.title}</p>
+                <p>Email: {props.email}</p>
             </div>
         </div>
     )
